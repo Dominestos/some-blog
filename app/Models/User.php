033b834696852ed52d428445a6ac7a96a750af66
Dominik <whitetorretohatbach@gmail.com>
2023-11-15 14:47:28 +0300

    realized sending messages with Notification class through queues

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Notifications\SendVerifyWithQueueNotification;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -27,7 +28,7 @@ class User extends Authenticatable implements MustVerifyEmail
         'name',
         'email',
         'password',
-        'role'
+        'role',
     ];
 
     /**
@@ -57,4 +58,10 @@ class User extends Authenticatable implements MustVerifyEmail
             self::ROLE_READER => 'Reader',
         ];
     }
+
+    public function sendEmailVerificationNotification()
+    {
+        $this->notify(new SendVerifyWithQueueNotification());
+    }
+
 }
